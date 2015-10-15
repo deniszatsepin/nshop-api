6b2e0e2e6f15fde615565b2c72477f93469446d7
Denis Zatsepin <denis@zatsepin.spb.ru>
2015-10-15 12:33:21 +0300

    Minor changes

--- src/api/users/lib/user_controller.js
+++ src/api/users/lib/user_controller.js
@@ -8,14 +8,19 @@ var UsersModel = models.Users;
 
 class User {
 
-  *controller(email, password) {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  *initialize() {
     try{
       var model = yield UsersModel.findOne({
         where: {
-          email: email
+          email: this.email
         }
       });
-      if (model.authenticate(password)) {
+      if (model.authenticate(this.password)) {
         this.model = model;
       } else {
         throw new Error('Password didn\'t match');
@@ -28,24 +33,13 @@ class User {
   static *create(email, password) {
     // TODO: we need to notify system about user creation, to send email to the user,
     //and maybe for other things
-    var user = UsersModel.build({
+    var model = UsersModel.build({
       email: email,
       password: password
     });
 
-    try {
-      user = yield user.save();
-    } catch(e) {
-      if (e instanceof user.sequelize.ValidationError) { //if validation errors, show them to the user
-        this.body = {
-          errors: e.errors
-        };
-        this.response.status = 422;
-        return;
-      } else { //if other errors, throw them up
-        throw e;
-      }
-    }
+    user = yield user.save();
+    this.model = model;
   }
 
 
